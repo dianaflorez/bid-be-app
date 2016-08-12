d2d3e3235bf4b98a7a3ea8006e5bccf6eb55b993
Diana Florez <dianaflorezbravo@gmail.com>
2016-08-12 11:17:47 -0500

    Update hello world message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World, from Heroku!');
 });
 
 app.listen(app.get('port'), function () {
